9936c8bb06c00bd7731dff4eab9fc121395e0e62
Ayush Bansal <bansalayush2345@gmail.com>
2026-05-28 10:27:38 +0530

    Update ReverseLinkedList.java

--- ReverseLinkedList.java
+++ ReverseLinkedList.java
@@ -19,50 +19,51 @@
 }
  */
 
-class Solution {
-    public ListNode reverseList(ListNode head) {
+
 
 //   .............................................      brute force     .................................................
-       // /* store the list in the arraylist and change the value of the nodes while
-        // traversing from the back of the array
+// /* store the list in the arraylist and change the value of the nodes while
+// traversing from the back of the array
+//         	class Solution {
+//         	public ListNode reverseList(ListNode head) {
+//        	List<Integer> nodeValueList = new ArrayList<>();
+//        	ListNode tempHead = head;
 
-        // List<Integer> nodeValueList = new ArrayList<>();
-        // ListNode tempHead = head;
+ //        	while(tempHead != null){
 
-        // while(tempHead != null){
+ //        		nodeValueList.add(tempHead.val);
+ //        		tempHead = tempHead.next;
+	
+ //        }
 
-        // nodeValueList.add(tempHead.val);
-        // tempHead = tempHead.next;
+ //        now traverse from the back of the list and change the value of list form the
+ //        starting
 
-        // }
+ //        int idx = nodeValueList.size() - 1;
+ //        tempHead = head;
 
-        // now traverse from the back of the list and change the value of list form the
-        // starting
+ //        while(tempHead != null){
+ //        		tempHead.val = nodeValueList.get(idx);
+ //        		idx--;
+ //        		tempHead = tempHead.next;
+ //        }
 
-        // int idx = nodeValueList.size() - 1;
-        // tempHead = head;
+ //         return head;
 
-        // while(tempHead != null){
-        // tempHead.val = nodeValueList.get(idx);
-        // idx--;
-        // tempHead = tempHead.next;
-        // }
-
-        // return head;
-
-        // TC --> 2 * O(N) == O(N) as we traversed the list 2 times
-        // SC --> O(N) as we stored the values of linked list in another list
+ //        // TC --> 2 * O(N) == O(N) as we traversed the list 2 times
+ //        // SC --> O(N) as we stored the values of linked list in another list
        
-    ......................................... optimization .........................................
+ //    ......................................... optimization .........................................
 		
-        // we have to traverse the whole linkedlist to get the all values but we can
-        // optimize space form O(N) to O(1)
-        //  example --
-        // 1 -> 2 -> 3 -> 4 -> 5
-        // 1 <- 2 <- 3 <- 4 <- 5
+ //        // we have to traverse the whole linkedlist to get the all values but we can
+ //        // optimize space form O(N) to O(1)
+ //        //  example --
+ //        // 1 -> 2 -> 3 -> 4 -> 5
+ //        // 1 <- 2 <- 3 <- 4 <- 5
 
-        // observation just need to reverse the arrow and we are done
-
+ //        // observation just need to reverse the arrow and we are done
+class Solution {
+    public ListNode reverseList(ListNode head) {
         ListNode prev = null;
         ListNode curr = head;
         ListNode ahead = head.next;
@@ -78,9 +79,8 @@ class Solution {
         }
         curr.next = prev;
         return curr;
-        // TC -> O(N) --> traversed the list only once
+    }
+}
+		// TC -> O(N) --> traversed the list only once
         // SC -> O(1) -->no extra space is used other than some constant variables so
         // overall reduced complexity is O(1)
-    }
-
-}
